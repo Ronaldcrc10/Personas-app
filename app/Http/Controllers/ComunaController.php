f75db74fa875e647a20e98230bf2f5d084b0fbc7
Ronaldo Collo <ronaldcollo172@gmail.com>
2024-09-30 19:08:29 -0500

    Eliminar Comuna

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -90,15 +90,26 @@ class ComunaController extends Controller
     {
         //
     }
-
+//____________________________________________________________________________
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Response  
      */
     public function destroy($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', "=", 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
+
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 }
+
+
+
